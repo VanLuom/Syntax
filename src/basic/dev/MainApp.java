f0af0a5f6701692c26cf513a4d08c200ef6ab88a
Vanluom <lluom14@gmail.com>
2021-10-27 00:22:22 +0700

    first commit

--- src/basic/dev/MainApp.java
+++ src/basic/dev/MainApp.java
@@ -1,10 +1,31 @@
 package basic.dev;
-
+/**
+ * Ghi chú (comment nhiều dòng)
+ * @author VanLuom
+ * 1. Tên lớp trùng tên file
+ * 2. Tên lớp chữ cái đầu viết hoa. Nếu từ ghép thì những chữ cái đầu viết hoa
+ * 4. Tên lớp phải có ý nghĩa. Ví dụ: class Peson : lớp mô tả con người
+ * - Interace: Phải có tiền tố I. Ví dụ: IListẻner
+ * - Abstract: Phải có tiền tố Abstract. Ví dụ:AbstractShape
+ */
 public class MainApp {
-
+	//Comment 1 dòng cách 1. Phím nóng Ctr + /
+	/*Conmmen 1 dòng cách 2*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello World");
+		/**
+		 * Cách đặt tên biến:
+		 * 1. Không chưa ký tự đặc biệt !@#$%^&*()_+=
+		 * 2. Không chưa khoảng trắng 
+		 * 3. Viết thường
+		 * 4. Không được bắt đầu bằng số 
+		 */
+		int number;
+		String nameSchool;
+		
 	}
-
+	/*
+	 * tên phương thức tuân theo quy tắc cameiCase
+	 */
 }
